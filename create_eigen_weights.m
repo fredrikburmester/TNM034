0da821575bd87a55d87dd0f7282f85284053efdc
Fredrik Burmester <fredrik.burmester@gmail.com>
2020-12-11 11:31:24 +0100

    add highboostfilter

--- create_eigen_weights.m
+++ create_eigen_weights.m
@@ -11,9 +11,9 @@ function [weights, meanFace, u] = create_eigen_weights()
         im_path = './DB1/' + string(f);
         RGB = imread(im_path);
         RGB = cropImage(RGB);
-%         imshow(RGB)
         grayImage = im2gray(im2double(RGB));
-%         grayImage = histeq(grayImage);
+        
+        grayImage = highboostfilter(grayImage);
         image_vector(:,count) = grayImage(:);
         count = count + 1;
     end
@@ -23,17 +23,21 @@ function [weights, meanFace, u] = create_eigen_weights()
 
     % Subtract mean
     A = image_vector-meanFace;
-
+    
     % Covariance 
     C1 = A'*A;
     [V, ~] = eig(C1);
     u = A*V;
-
+    
     % Normalization
     for i=1:16
         u(:,i) = u(:,i)/norm(u(:,i));
     end
-    
+%     for i=1:16
+%          u(:,i) = rescale(u(:,i),0,1);
+%     end
+   
+
 %     for i=1:16
 %         u(:,i) = sort(u(:,i));
 %     end

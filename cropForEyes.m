921159885a500fae7da11b9421c736d992367892
Fredrik Burmester <fredrik.burmester@gmail.com>
2020-12-13 13:03:44 +0100

    changed variable names

--- cropForEyes.m
+++ cropForEyes.m
@@ -1,23 +1,22 @@
 function [out] = cropForEyes(img, x1, y1, x2, y2)
+    
+    % Switch variables to x1 is left eye, and x2 is right eye
     if(x2 < x1) 
         temp = x1;
         x1 = x2;
         x2 = temp;
-        
-        temp = y1;
         y1 = y2;
-        y2 = temp;
     end
         
     deltaX = abs(x1-x2);
     
     scale = 100/deltaX;
     
-    temp = imresize(img, scale);
+    img_temp = imresize(img, scale);
     
     x1 = floor(x1 * scale);
     y1 = floor(y1 * scale);
-    s = size(temp);
+    s = size(img_temp);
     
     b1 = y1-49;
     b2 = y1+150;
@@ -25,7 +24,7 @@ function [out] = cropForEyes(img, x1, y1, x2, y2)
     b4 = x1+150;
     
     if (b3 > 0) & (b1 > 0) & (b2 < s(1)) & (b4 < s(2)) 
-        out = temp(b1:b2, b3:b4,:);
+        out = img_temp(b1:b2, b3:b4,:);
     else
         out = 0;
     end

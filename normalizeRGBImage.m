612b4d0e7d4a12f058f1bb17ae5334c15fd333f2
Fredrik Burmester <fredrik.burmester@gmail.com>
2020-12-13 13:04:32 +0100

    Update normalizeRGBImage.m

--- normalizeRGBImage.m
+++ normalizeRGBImage.m
@@ -1,20 +1,14 @@
 function [normalizedImage] = normalizeRGBImage(in)
 
-    normalizedImage = in;
-    
-    r = in(:,:,1);
-    g = in(:,:,2);
-    b = in(:,:,3);
+%     r = in(:,:,1);
+%     g = in(:,:,2);
+%     b = in(:,:,3);
+% 
+%     m = ( mean(r(:)) + mean(g(:)) + mean(b(:)) ) / 3;
+%     
+%     in = in * (128 / m);
 
-    m = (mean(r(:)) + mean(g(:)) + mean(b(:))) / 3;
-
-%     if (m < 128 || m > 128 )
-%         normalizedImage = in * (128 / m); 
-%         return;
-%     end
-     
-    normalizedImage =  imadjust(normalizedImage,stretchlim(normalizedImage));
-%     normalizedImage = (in - min(min(in))) / (max(max(in)) - min(min(in)) );
+    normalizedImage = imadjust(in,stretchlim(in));
 
 end
 

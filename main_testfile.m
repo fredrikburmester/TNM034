7dc6eda472037841b6bae5bebb505c5bbd724329
Fredrik Burmester <fredrik.burmester@gmail.com>
2020-12-15 11:27:41 +0100

    Update main_testfile.m

--- main_testfile.m
+++ main_testfile.m
@@ -3,16 +3,18 @@ clear
 clf
 clc
 
-files=dir('./DB2/*.jpg');
+files=dir('./DB1/*.jpg');
 files =  {files.name};
 
 % Test all files in a dataset against our dataset based on DB1
 success = 0;
 attempts = 0;
-for j = 1:10
+false_positive = 0;
+for j = 1:500
+    j
     for file = files
         attempts = attempts+1;
-        path = './DB2/' + string(file);
+        path = './DB1/' + string(file);
         im = imread(path);
         
         r1 = 0.7 + (1.3-0.7)*rand(1);
@@ -24,13 +26,27 @@ for j = 1:10
         im = imresize(im,r3);
     
         [id, value] = TNM034(im);
-        if id ~= 0
-            success = success + 1;
+        C = strsplit(string(file),'_');
+        C = strsplit(C(2),'.');
+        C = C(1);
+        
+        if id < 10
+            index = '0' + string(id);
+        else
+            index = string(id);
         end
-      fprintf('%s, %i, %.4f \n', string(file), id, value);
+        
+        if index == C
+            success = success + 1;
+        elseif (index ~= C && id > 0)
+            false_positive = false_positive + 1;
+%             fprintf('False positives: %i %.4f \n', false_positive, value);
+        end
+%       fprintf('%s, %i, %.4f \n', string(file), id, value);
     end
 end
 fprintf('Success rate: %.4f \n', success/attempts);
+fprintf('False positives: %i \n', false_positive);
 
 %%
 

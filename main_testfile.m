5e391cb59c99e1c6536ea781f333da51e2264ab8
Fredrik Burmester <fredrik.burmester@gmail.com>
2020-12-15 11:43:40 +0100

    Update main_testfile.m

--- main_testfile.m
+++ main_testfile.m
@@ -3,6 +3,7 @@ clear
 clf
 clc
 
+% Load the folder DB1
 files=dir('./DB1/*.jpg');
 files =  {files.name};
 
@@ -10,13 +11,16 @@ files =  {files.name};
 success = 0;
 attempts = 0;
 false_positive = 0;
+
+% For all 16 images in DB1, test each image with random variations 500 times against DB1
 for j = 1:500
-    j
+    disp(j) % print current loop iteration
     for file = files
         attempts = attempts+1;
         path = './DB1/' + string(file);
         im = imread(path);
         
+        % Create random variations in the images
         r1 = 0.7 + (1.3-0.7)*rand(1);
         r2 = -5 + (5-(-5))*rand(1);
         r3 = 0.9 + (1.1-0.9)*rand(1);
@@ -40,18 +44,10 @@ for j = 1:500
             success = success + 1;
         elseif (index ~= C && id > 0)
             false_positive = false_positive + 1;
-%             fprintf('False positives: %i %.4f \n', false_positive, value);
+%             fprintf('False positives: %i %.4f \n', false_positive, value); % For printing false positives during the test
         end
-%       fprintf('%s, %i, %.4f \n', string(file), id, value);
+%       fprintf('%s, %i, %.4f \n', string(file), id, value); % Used for printing the file, id and value during the test
     end
 end
 fprintf('Success rate: %.4f \n', success/attempts);
 fprintf('False positives: %i \n', false_positive);
-
-%%
-
-im = imread('./DB1/db1_01.jpg');
-% im = imrotate(im,5,'bilinear','crop');
-% im = grayWorld(im)
-% figure, imshow(im)
-[id,value] = TNM034(im)
